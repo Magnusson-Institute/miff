a7b83338bf75af33904d74892182157b4c7b9e93
Peter <peter@magnusson.institute>
2020-12-14 17:17:28 -0800

    removed attempts to load quilt python libraries - don't work

--- setup_patch_environment.py
+++ setup_patch_environment.py
@@ -1,8 +1,13 @@
 import os
 import sys
-from quilt.push import Push
+
+# PSM - ok this stuff isn't used yet, so don't import
+# import quilt
+# from quilt.push import quilt.push
+# from quilt import push
 
 PATCH_DIR = os.getcwd() + "/patches"
+
 # The argument should be the directory of the Firefox source code
 SOURCE_DIR = os.getcwd() + "/" + sys.argv[1]
 
@@ -21,7 +26,9 @@ for n in patchlist:
         fout.write(line.replace('mozilla-release', sys.argv[1]))
     fin.close
     fout.close
+
 # Copy the series file into the new directory
+
 series = [n for n in os.listdir(PATCH_DIR) if "series" in n]
 fin = open(PATCH_DIR+"/"+series[0], "r")
 fout = open(SOURCE_DIR+"_patches/"+series[0], "w")
@@ -33,10 +40,13 @@ fout.close
 # Edit the quilt_patch file to point to the new patch directory
 QUILT_DIR = os.getcwd() + "/.pc"
 fout = open(QUILT_DIR + "/.quilt_patches", "w")
-fout.write(argv[1]+"_patches")
+fout.write(sys.argv[1]+"_patches")
 fout.close
+
+
 # Apply the patches with Quilt
-#push = Push(os.getcwd(), QUILT_DIR, SOURCE_DIR+"_patches")
+
+#push = quilt.push(os.getcwd(), QUILT_DIR, SOURCE_DIR+"_patches")
 #try:
 #    push.apply_all()
 #except:
